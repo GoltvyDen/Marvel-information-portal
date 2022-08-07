eeb647bd710902b19685cc42ee6d799fdd36a408
GoltvyDen <Den.goltvyanskiy2109@gmail.com>
2022-08-08 00:07:41 +0300

    added characters list

--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -7,11 +7,6 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
-
     state = {
         char: {},
         loading: true,
@@ -20,10 +15,25 @@ class RandomChar extends Component {
 
     marvelService = new MarvelService();
 
-    onCharLoader = (char) => {
+    componentDidMount() {
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 10000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    onCharLoaded = (char) => {
         this.setState({
             char, 
             loading: false
+        });
+    }
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true
         });
     }
 
@@ -36,9 +46,10 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
-            .then(this.onCharLoader)
+            .then(this.onCharLoaded)
             .catch(this.onError);
     }
         
@@ -61,7 +72,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button onClick={this.updateChar} className="button button__main">
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -73,10 +84,13 @@ class RandomChar extends Component {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
+    
+    let imgStyle = {};
+    thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? imgStyle =  {'object-fit' : 'contain'} : imgStyle = {'object-fit' : 'cover'};
 
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
