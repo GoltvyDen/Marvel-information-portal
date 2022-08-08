42ed74f6671fbf9c7f35c7ba17fca99f660123ce
GoltvyDen <Den.goltvyanskiy2109@gmail.com>
2022-08-08 23:45:13 +0300

    added pagination

--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -1,6 +1,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=d655cee05f0738487255aeaab344f82a'
+    _baseOffset = 210
 
     getResourse = async (url) => {
         let res = await fetch(url);
@@ -12,8 +13,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
@@ -36,4 +37,3 @@ class MarvelService {
 }
 
 export default MarvelService;
-
