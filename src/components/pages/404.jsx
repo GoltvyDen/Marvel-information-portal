72e2989d55d8e86e677b3cc291072a52ca1f00d6
GoltvyDen <Den.goltvyanskiy206@gmail.com>
2022-10-31 01:06:00 +0200

    added SEO optimization

--- src/components/pages/404.jsx
+++ src/components/pages/404.jsx
@@ -1,9 +1,17 @@
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import {Link} from 'react-router-dom';
+import { Helmet } from "react-helmet";
 
 const Page404 = () => {
     return (
         <div>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Error. Page not found"
+                />
+                <title>Page not found</title>
+            </Helmet>
             <ErrorMessage/>
             <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '30px', 'color': '#9f0013'}}>Apparently this page belonged to the unlucky half</p>
             <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px', 'textDecoration': 'underline'}} to='/'>Back to the main page</Link>
